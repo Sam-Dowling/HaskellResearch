399550a93909d4b9dd037725c1295185464a20a0
sam dowling <sam.dowling@students.ittralee.ie>
2016-02-11 13:27:38 +0000

    updated catamorphism example

--- Monads/catamorphism.hs
+++ Monads/catamorphism.hs
@@ -1,23 +1,42 @@
 type Algebra f a = f a -> a 
 newtype Mu f = InF { outF :: f (Mu f) } 
+
 cata :: Functor f => Algebra f a -> Mu f -> a 
 cata f = f . fmap (cata f) . outF
 
+---------------------------------------------------
+
 data StrF x = Cons Char x | Nil 
 type Str = Mu StrF
+
 instance Functor StrF where     
     fmap f (Cons a as) = Cons a (f as)     
     fmap f Nil = Nil
     
-cata_length :: Str -> Int 
-cata_length = cata phi where     
+clength :: Str -> Int 
+clength = cata phi where     
     phi (Cons a b) = 1 + b     
     phi Nil = 0
     
-foo=InF { outF = Cons 'f'
-        (InF { outF = Cons 'o'
-            (InF { outF = Cons 'o'
-                (InF { outF = Nil })})})}
+sam= InF { outF = Cons 'S'
+    (InF { outF = Cons 'a'
+    (InF { outF = Cons 'm'
+    (InF { outF = Nil })})})}
     
-                
-example = cata_length foo
+------------------------------------------------------
+
+data TreeF x = Node Int [x]
+type Tree = Mu TreeF
+
+instance Functor TreeF where
+  fmap f (Node e xs) = Node e (fmap f xs)
+
+cdepth :: Tree -> Int
+cdepth = cata phi where
+  phi :: Algebra TreeF Int
+  phi (Node x sons) = 1 + foldr max 0 sons
+
+samTree = InF { outF = Node 4 [ 
+          InF { outF = Node 1 [
+          InF { outF = Node 3 [
+          InF { outF = Node 2 [] }]}]}]}
