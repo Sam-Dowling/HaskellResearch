e46d313452e7ed1dbeaf33d38da4465089a42239
sam dowling <sam.dowling@students.ittralee.ie>
2016-02-17 11:53:39 +0000

    Updated persistent storage example

--- Monads/persistent.hs
+++ Monads/persistent.hs
@@ -2,17 +2,19 @@
 {-# LANGUAGE OverloadedStrings, GADTs, FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses, GeneralizedNewtypeDeriving #-}
 
-import Data.Text
-import Data.Conduit
-import Data.Conduit.List as CL
-import Control.Monad.IO.Class
-import Database.Persist
-import Database.Persist.Sqlite
-import Database.Persist.TH
+import Data.Text (Text)
+import Database.Esqueleto
+import Database.Persist.Sqlite (runSqlite, runMigrationSilent)
+import Database.Persist.TH (mkPersist, mkMigrate, persistLowerCase,
+    share, sqlSettings)    
+import Database.Persist.Sql (insert)
+import Control.Monad.IO.Class (liftIO)
 
 share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
 User
-    username Text
+    name     Text
+    email    Text
+    EmailKey email
     password Text
     isadmin  Bool
     deriving Show
@@ -24,24 +26,49 @@ Blog
 |]
 
 
-main = runSqlite ":memory:" $ do
+main = runSqlite ":memory:" $ do -- :memory:
     buildDB
-    selectUser "Sam"
-    dumpTable
+    selectUserBlogs "sam@mail.com"
+    countBlogs
+    updateEmail "sam@mail.com" "sam@mynewemail.net" "alice@mail.org"
 
-selectUser u = do
-    user <- selectList [UserUsername ==. u][]
-    liftIO $ print $ user
+
+selectUserBlogs u = do
+    blogs <- select $ from $ \(a, t) -> do
+            where_ (a ^. UserEmail ==. val u &&. t ^. BlogAuthor ==. a ^. UserId)
+            return (t ^. BlogTitle)
+    liftIO $ print blogs
+
+
+countBlogs = do
+    blogs <- select $ from $ \(a, t) -> do
+        where_ (t ^. BlogAuthor ==. a ^. UserId)
+        groupBy (a ^. UserId)
+        let cnt = countRows :: SqlExpr (Value Int)
+        orderBy [desc cnt]
+        return (a ^. UserEmail, cnt)
+    liftIO $ print blogs
+
+
+updateEmail old new del = do
+
+    update $ \a -> do
+        set a [UserEmail =. val new]
+        where_ (a ^. UserEmail ==. val old)
+    
+    delete $ from $ \t ->
+        where_ (t ^. UserEmail ==. val del &&. t ^. UserIsadmin ==. val False)
+    
+    auths <- select $ from $ \a -> return a
+    liftIO $ mapM_ (print . userEmail . entityVal) auths
+
 
 buildDB = do
     runMigrationSilent migrateTables
-    sam <-   insert $ User "Sam" "password" True
-    alice <- insert $ User"Alice" "pass" False
-    bob <-   insert $ User "Bob" "mypass" True
+    sam <-   insert $ User "Sam" "sam@mail.com" "password" True
+    alice <- insert $ User "Alice" "alice@mail.org" "pass" False
+    bob <-   insert $ User "Bob" "bob@bobmail.ie" "mypass" True
     insert $ Blog "Blog1" "blogurl.com" sam
     insert $ Blog "Blog2" "blogurl.net" sam
     insert $ Blog "Blog3" "blogurl.org" alice
     insert $ Blog "Blog4" "blogurl.ie" bob
-
-dumpTable = rawQuery "select * from Blog" [] $$ CL.mapM_ (liftIO . print)
-    
